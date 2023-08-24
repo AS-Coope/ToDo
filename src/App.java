f9f60ca119dab485e5a846a4621c0e67180c8fab
AS-Coope <deltazirconium@gmail.com>
2023-08-23 19:45:09 -0500

    Create connection to database

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.sql.Connection;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        DAO dao = new DAO();
+        Connection dbConn = dao.connectToDb("todo", "postgres", "password");
     }
 }
--- src/DAO.java
+++ src/DAO.java
@@ -1,3 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DAO {
 
+    public Connection connectToDb(String db, String user, String password) {
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + db, user, password);
+
+            if (conn != null) {
+                System.out.println("Connection established with " + db + "!");
+            } else {
+                System.out.println("Connection to " + db + " was unsuccessful!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error connecting to the database! " + e);
+        }
+
+        return conn;
+    }
 }
